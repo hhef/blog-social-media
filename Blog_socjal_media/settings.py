6fbf481acfc42a64abce40b2a743cd32bbfccd85
heff <ksuder7@gmail.com>
2019-10-13 13:07:06 +0200

    add database and create superuser

--- Blog_socjal_media/settings.py
+++ Blog_socjal_media/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Blog_socjal_media.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'HOST': '127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': 'jajecznica12',
     }
 }
 
