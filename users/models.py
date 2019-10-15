f02cacc4ca558364314f083b1dfa47d1cf38d71b
heff <ksuder7@gmail.com>
2019-10-15 15:21:50 +0200

    add image changes after upload profile
    model will automaticly resize image for 250 x 250
    
    px after upload for space save in file system
    
    old pic will be deleted after image upload

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        image = Image.open(self.image.path)
+        if image.height > 250 or image.width > 300:
+            new_image_size = (250, 250)
+            image.thumbnail(new_image_size)
+            image.save(self.image.path)
