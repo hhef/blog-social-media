da17003ed18904cc963d813e63c933cf0729ba04
heff <ksuder7@gmail.com>
2019-10-16 12:24:16 +0200

    change home page to class based view
    create post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        "posts": posts
-    }
-    return render(request, "blog/home.html", context)
+# def home(request):
+#     posts = Post.objects.all()
+#     context = {
+#         "posts": posts
+#     }
+#     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    ordering = ['-date_posted']
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
