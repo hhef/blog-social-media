756f82ae8f1aaad96291c4233097b1e5276b1bb5
heff <ksuder7@gmail.com>
2019-10-24 12:49:26 +0200

    add search option through posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, \
+    SearchListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('search/', SearchListView.as_view(), name='search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 class PostListView(ListView):
@@ -22,6 +23,24 @@ class UserPostListView(ListView):
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class SearchListView(ListView):
+    model = Post
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        queryset_list = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(author__username__icontains=query) |
+            Q(author__first_name__contains=query) |
+            Q(author__last_name__icontains=query)
+        ).distinct()
+        return queryset_list.order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
